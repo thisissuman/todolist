19b46e22612356f417ac70dacc1e978f978584b3
thisissuman <43521912+thisissuman@users.noreply.github.com>
2024-03-30 20:30:54 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { TodoWrapperLocalStorage } from "./components/ToDoWrapper";
+import "./App.css";
+function App() {
+  return (
+    <div className="App">
+      <TodoWrapperLocalStorage />
+    </div>
+  );
+}
+
+export default App;
